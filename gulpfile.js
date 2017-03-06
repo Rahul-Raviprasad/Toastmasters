051ca68e89011e140d8b443244ee0e498e1df881
rahul.raviprasad <rahul.raviprasad@gmail.com>
2017-03-06 17:31:17 +0530

    added browserSync and created dev task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 var gulp = require('gulp');
 var _  = require('lodash');
 var fs = require('fs');
-//var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
 
 // Default task
 gulp.task('default', []);
@@ -11,4 +11,18 @@ gulp.task('default', []);
 gulp.task('bootstrap', function() {
   return gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
              .pipe(gulp.dest('vendor/bootstrap'));
+});
+
+// browser sync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    }
+  });
+});
+
+// dev task
+gulp.task('dev', ['browserSync'], function() {
+  gulp.watch('*.html', browserSync.reload);
 });
