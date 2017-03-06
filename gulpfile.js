2f6acf773f9d3e826a1540a28bb490f1a608e6e3
rahul.raviprasad <rahul.raviprasad@gmail.com>
2017-03-06 16:59:41 +0530

    added bootstrap as adev dependency and created a gulp task for the same

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 'use strict';
-
+var gulp = require('gulp');
 var _  = require('lodash');
 var fs = require('fs');
+//var browserSync = require('browser-sync').create();
+
+// Default task
+gulp.task('default', []);
+
+// Bootstrap task
+gulp.task('bootstrap', function() {
+  return gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+             .pipe(gulp.dest('vendor/bootstrap'));
+});
