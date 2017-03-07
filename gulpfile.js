bac942535edf3d9e2dbced4f4a3c4ab0dab29047
rahul.raviprasad <rahul.raviprasad@gmail.com>
2017-03-07 14:37:47 +0530

    added main css file and task to minify it

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 var gulp = require('gulp');
-var _  = require('lodash');
-var fs = require('fs');
+// var _  = require('lodash');
+// var fs = require('fs');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
 // Default task
@@ -11,6 +13,17 @@ gulp.task('default', []);
 gulp.task('bootstrap', function() {
   return gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
              .pipe(gulp.dest('vendor/bootstrap'));
+});
+
+//minify css
+gulp.task('minify-css', function() {
+  return gulp.src('css/main.css')
+             .pipe(cleanCSS({compatibility: 'ie8'}))
+             .pipe(rename({suffix: '.min'}))
+             .pipe(gulp.dest('css'))
+             .pipe(browserSync.reload({
+               stream: true
+             }));
 });
 
 // browser sync task
@@ -24,5 +37,6 @@ gulp.task('browserSync', function() {
 
 // dev task
 gulp.task('dev', ['browserSync'], function() {
+  gulp.watch('css/*.css', ['minify-css']);
   gulp.watch('*.html', browserSync.reload);
 });
