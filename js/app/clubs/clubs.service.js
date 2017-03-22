3dd92cda8f43e753960cd4b572f0960f199f01f0
rahul.raviprasad <rahul.raviprasad@gmail.com>
2017-03-22 22:42:08 +0530

    added authentication service

--- js/app/clubs/clubs.service.js
+++ js/app/clubs/clubs.service.js
@@ -1,10 +1,10 @@
 angular
-  .module('app.clubsService', [])
+  .module('app.clubsService', ['app.common.auth'])
   .factory('ClubService', ClubService);
 
-ClubService.$inject = ['$q'];
+ClubService.$inject = ['$q', '$http', 'AuthenticationService'];
 
-function ClubService($q) {
+function ClubService($q, $http, AuthenticationService) {
   var vm = this;
 
   vm.getClubs = getClubs;
@@ -14,21 +14,24 @@ function ClubService($q) {
   /////////----------------------------------------------
 
   function getClubs() {
-    return $q.when({
-      clubs: [
-        {
-          name: 'Hyderabad',
-          description: "A cool place to hangout and learn"
-        },
-        {
-          name: 'Data Science group',
-          description: 'meet some of the most enthusiatic data scientist'
-        },
-        {
-          name: 'Football group',
-          description: 'play soccer and get some cardio done as well'
-        }
-      ]
-    });
+    AuthenticationService.SetCredentials('username', 'password');
+
+    return $http.get('https://rahulraviprasad.service-now.com/api/now/table/x_snc_toastmaster1_clubs?sysparm_fields=name%2Cshort_description');
+    // return $q.when({
+    //   clubs: [
+    //     {
+    //       name: 'Hyderabad',
+    //       description: "A cool place to hangout and learn"
+    //     },
+    //     {
+    //       name: 'Data Science group',
+    //       description: 'meet some of the most enthusiatic data scientist'
+    //     },
+    //     {
+    //       name: 'Football group',
+    //       description: 'play soccer and get some cardio done as well'
+    //     }
+    //   ]
+    // });
   }
 }
--- js/app/common/base64.service.js
+++ js/app/common/base64.service.js
@@ -89,5 +89,5 @@
     };
 
     /* jshint ignore:end */
-});
+}
 })();
